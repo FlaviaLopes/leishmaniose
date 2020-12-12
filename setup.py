cc2e020c625287d342aab186427c2a3b6ac7d6bd
Flávia <flavithalopes@hotmail.com>
2020-12-12 15:30:35 -0300

    Adiciona makefile e outros arquivos
    makefile para ajudar no processo de criação de virtual environment e execução via linha de comando.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploração dos dados históricos do SINAN (SUS) especificamente das Leishmanioses (LT e LV).',
+    author='FlaviaLopes',
+    license='Apache-2.0 License',
+)
